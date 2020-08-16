219f75b18e296e6c96bae82541c8200979b90f4b
pavan kumar <60856229+pavan-2001@users.noreply.github.com>
2020-08-16 11:51:15 +0530

    Update HS08TEST.cpp

--- HS08TEST.cpp
+++ HS08TEST.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include<iomanip>
 using namespace std;
-
 int main() 
 {
-
-	int a;
-	float b;
-	cin>>a>>b;
-	if(a%5==0&&a<=b-0.50)
-  b=b-a-0.50;
-  	cout<<fixed<<setprecision(2)<<b;
-	return 0;
+int a;
+float b;
+cin>>a>>b;
+if(a%5==0&&a<=b-0.50)
+b=b-a-0.50;
+cout<<fixed<<setprecision(2)<<b;
+return 0;
 }
